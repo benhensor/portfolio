a2ba89ada4877e1fed2ef7af8100f4aa7d70e341
benhensor <benhensor@gmail.com>
2024-07-09 16:40:55 +0100

    deploying server to netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -57,7 +57,7 @@ export default function Contact() {
       }
       setFormErrors({})
       setButtonText('SENDING...')
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
